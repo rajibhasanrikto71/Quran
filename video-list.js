0f447c32f999070768756879b286ad23d0c72905
Villain <83833885+rajibhasanrikto71@users.noreply.github.com>
2022-04-03 22:21:17 +0600

    Add files via upload

---
+++ video-list.js
@@ -0,0 +1,142 @@
+let allVideos = [
+   {
+      name: "Class 1",
+      src: "https://drive.google.com/file/d/1wTcSYW1pr_-FeDH_DKp0VgemCZHF8HJ1/preview",
+      id: "vid_1"
+   },
+   {
+      name: "Class 2",
+      src: "hhttps://drive.google.com/file/d/1znphiumVhPofuvg5yeNdZycPos0Y3zQV/preview",
+      id: "vid_2"
+   },
+   {
+      name: "Class 3",
+      src: "https://drive.google.com/file/d/1vgw6z-XIwe-Z8CUlDZcxe9hg9iFkPS3q/preview",
+      id: "vid_3"
+   },
+   {
+      name: "Class 4",
+      src: "https://drive.google.com/file/d/1-4pJH9U2itXi17bWWFPth8UYiaLjhWOH/preview",
+      id: "vid_4"
+   },
+   {
+      name: "Class 5",
+      src: "https://drive.google.com/file/d/1uqIb2Ei7OyE153OowbnO-_oxxEACDp6U/preview",
+      id: "vid_5"
+   },
+   {
+      name: "Class 6",
+      src: "https://drive.google.com/file/d/1y03fXH0BKmDBk2LZpG6cCeOSeSlEzRsa/preview",
+      id: "vid_6"
+   },
+   {
+      name: "Class 7",
+      src: "https://drive.google.com/file/d/1yBcZmF7DibPL9gheYh5IBYttejDDXoe7/preview",
+      id: "vid_7"
+   },
+   {
+      name: "Class 8",
+      src: "https://drive.google.com/file/d/1--Ft1Tt3FHA2hWt_1-qZTrzmyahM40lC/preview",
+      id: "vid_8"
+   },
+   {
+      name: "Class 9",
+      src: "https://drive.google.com/file/d/1z6HKyPPyc2Rn_AXubRVTZGSjOugZ7BUM/preview",
+      id: "vid_9"
+   },
+   {
+      name: "Class 10",
+      src: "https://drive.google.com/file/d/1uHWQQcKw_2bNaDQNcXHOsDUwHrPtQZWF/preview",
+      id: "vid_10"
+   },
+   {
+      name: "Class 11",
+      src: "https://drive.google.com/file/d/1xLc_8TvDRHh3KE3VVRFuBhZEmz0AiePt/preview",
+      id: "vid_11"
+   },
+   {
+      name: "Class 12",
+      src: "https://drive.google.com/file/d/1zsCF7mCcEPLRYvPbuTagBohQyBR1zlhh/preview",
+      id: "vid_12"
+   },
+   {
+      name: "Class 13",
+      src: "https://drive.google.com/file/d/1w1vQUJ6BRoV3kVqNkCGcvshulfKKb6XX/preview",
+      id: "vid_13"
+   },
+   {
+      name: "Class 14",
+      src: "https://drive.google.com/file/d/1uap_BYtPl0TFEE_RzXkLe4extA1oUzIW/preview",
+      id: "vid_14"
+   },
+   {
+      name: "Class 15",
+      src: "https://drive.google.com/file/d/1vwCXxq2R2cb60xiaeZRUEF6bmRZoFdgs/preview",
+      id: "vid_15"
+   },
+   {
+      name: "Class 16",
+      src: "https://drive.google.com/file/d/1wqlqEf7RqL7MwyTO2ullsLdPrlwDEADN/preview",
+      id: "vid_16"
+   },
+   {
+      name: "Class 17",
+      src: "https://drive.google.com/file/d/1v64owM0vxS0-J3-jVQdWrr4gHxEkwQW8/preview",
+      id: "vid_17"
+   },
+   {
+      name: "Class 18",
+      src: "https://drive.google.com/file/d/1-BXTiTxVS4eXeUcte0SU2Rt-Lwv6e-Jx/preview",
+      id: "vid_18"
+   },
+   {
+      name: "Class 19",
+      src: "https://drive.google.com/file/d/1vShUkShYxP2RIuXWDvnwpRnFI1xdx0bp/preview",
+      id: "vid_19"
+   },
+   {
+      name: "Class 20",
+      src: "https://drive.google.com/file/d/1ul145KQSip1BxbixdzSzXQkxoH8t4L2W/preview",
+      id: "vid_20"
+   },
+   {
+      name: "Class 21",
+      src: "https://drive.google.com/file/d/1yWch9GC4FnUFgqZTilDR0vz-z0xmox8K/preview",
+      id: "vid_21"
+   },
+   {
+      name: "Class 22",
+      src: "https://drive.google.com/file/d/1vI95uMLKeeEwQrdA4OKQVKaf1ULf2ibQ/preview",
+      id: "vid_22"
+   },
+   {
+      name: "Class 23",
+      src: "https://drive.google.com/file/d/1yiglRBqNadZg19hC-2Whv6eBfzYOEvfv/preview",
+      id: "vid_23"
+   },
+   {
+      name: "Class 24",
+      src: "https://drive.google.com/file/d/1z4uUW1H2X3kgqqTTL1bOOMldQBO6Xr3D/preview",
+      id: "vid_24"
+   },
+   {
+      name: "Class 25",
+      src: "https://drive.google.com/file/d/1xZTfrYWpJV2YddOmNo4guBJJ5OmV538w/preview",
+      id: "vid_25"
+   },
+   {
+      name: "Class 26",
+      src: "https://drive.google.com/file/d/1u9YfKwg1G_hL3QxJZJHoqHjdOP3KBVZG/preview",
+      id: "vid_26"
+   },
+   {
+      name: "Class 27",
+      src: "https://drive.google.com/file/d/1uTcCunSn4UmyXy7iovJGZ3STBhLan1gm/preview",
+      id: "vid_27"
+   },
+   {
+      name: "Class 28",
+      src: "https://drive.google.com/file/d/1yuKEvvsScLSQF77aQHoqsUz4ek2hhrpQ/preview",
+      id: "vid_28"
+   },
+]
